a52d1167fe74cc0ed1b37e925c86ae971abab057
tqk <1282995880@qq.com>
2019-05-17 20:03:42 +0800

    插入排序

--- src/main/java/sort/InsertionSort.java
+++ src/main/java/sort/InsertionSort.java
@@ -1,5 +1,7 @@
 package sort;
 
+import utils.RandomIntArray;
+
 /**
  * ___________ ________     ____  __.
  * \__    ___/ \_____  \   |    |/ _|
@@ -11,4 +13,25 @@ package sort;
  * @Date: 2019-05-17 19:32
  */
 public class InsertionSort {
+    public static int[] insertionSort(int[] arr) {
+        if (arr.length < 2) {
+            return arr;
+        }
+        int j;
+        for (int i = 1; i < arr.length; i++) {
+            int temp = arr[i];
+            for (j = i; j > 0 && arr[j - 1] > temp; j--) {
+                arr[j] = arr[j - 1];
+            }
+            arr[j]=temp;
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = RandomIntArray.getArray(0,1000,300);
+        RandomIntArray.display(arr);
+        InsertionSort.insertionSort(arr);
+        RandomIntArray.display(arr);
+    }
 }
