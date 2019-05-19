68264a70e56fd1c67e5597335130b3ce89c6d094
tqk <1282995880@qq.com>
2019-05-19 13:47:43 +0800

    数组中重复的数字

---
+++ src/main/java/offer/Three.java
@@ -0,0 +1,37 @@
+package offer;
+
+import utils.RandomIntArray;
+
+/**
+ * ___________ ________     ____  __.
+ * \__    ___/ \_____  \   |    |/ _|
+ * |    |     /  / \  \  |      <
+ * |    |    /   \_/.  \ |    |  \
+ * |____|    \_____\ \_/ |____|__ \
+ *
+ * @Author: tuqikang
+ * @Date: 2019-05-19 13:20
+ * 在一个长度为 n 的数组里的所有数字都在 0 到 n-1 的范围内。
+ * 数组中某些数字是重复的，但不知道有几个数字是重复的，也不知道每个数字重复几次。请找出数组中任意一个重复的数字。
+ * Input:
+ * {2, 3, 1, 0, 2, 5}
+ * Output:
+ * 2
+ */
+public class Three {
+    public static void main(String[] args) {
+        int[] arr = RandomIntArray.getArray(0, 10, 10);
+        RandomIntArray.display(arr);
+        for (int index = 0; index < arr.length; index++) {
+            while (index!=arr[index]){
+                if (arr[index]==arr[arr[index]]){
+                    System.out.println(arr[index]);
+                    return;
+                }
+                RandomIntArray.swap(arr,index,arr[index]);
+            }
+        }
+        System.out.println("无");
+
+    }
+}
