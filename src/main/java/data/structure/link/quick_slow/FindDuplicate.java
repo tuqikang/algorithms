30c431955832ac87ae1191b71379f660d162565e
tqk <1282995880@qq.com>
2019-06-19 14:47:30 +0800

    leetcode287

---
+++ src/main/java/data/structure/link/quick_slow/FindDuplicate.java
@@ -0,0 +1,44 @@
+package data.structure.link.quick_slow;
+
+/**
+ * █████▒█      ██  ▄████▄   ██ ▄█▀       ██████╗ ██╗   ██╗ ██████╗
+ * ▓██   ▒ ██  ▓██▒▒██▀ ▀█   ██▄█▒        ██╔══██╗██║   ██║██╔════╝
+ * ▒████ ░▓██  ▒██░▒▓█    ▄ ▓███▄░        ██████╔╝██║   ██║██║  ███╗
+ * ░▓█▒  ░▓▓█  ░██░▒▓▓▄ ▄██▒▓██ █▄        ██╔══██╗██║   ██║██║   ██║
+ * ░▒█░   ▒▒█████▓ ▒ ▓███▀ ░▒██▒ █▄       ██████╔╝╚██████╔╝╚██████╔╝
+ * ▒ ░   ░▒▓▒ ▒ ▒ ░ ░▒ ▒  ░▒ ▒▒ ▓▒       ╚═════╝  ╚═════╝  ╚═════╝
+ * ░     ░░▒░ ░ ░   ░  ▒   ░ ░▒ ▒░
+ * ░ ░    ░░░ ░ ░ ░        ░ ░░ ░
+ * ░     ░ ░      ░  ░
+ *
+ * @author ：涂齐康
+ * @date ：Created in 2019-06-19 13:58
+ * leetcode287
+ * @description：寻找重复的数字
+ * @modified By：
+ * @version:
+ */
+public class FindDuplicate {
+
+    public int findDuplicate(int[] nums) {
+        int slow = 0;
+        int fast = 0;
+        while (true) {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+            if (slow == fast) {
+                break;
+            }
+            if (nums[slow] == nums[fast]) {
+                return nums[slow];
+            }
+        }
+        int head = 0;
+        while (slow != head) {
+            slow = nums[slow];
+            head = nums[head];
+        }
+        return head;
+    }
+
+}
