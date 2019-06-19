4e601a58cbe7cdd0d88d49921d2514d88a3a3e83
tqk <1282995880@qq.com>
2019-06-19 13:55:23 +0800

    leetcode142

--- src/main/java/data/structure/link/quick_slow/DetectCycle.java
+++ src/main/java/data/structure/link/quick_slow/DetectCycle.java
@@ -12,44 +12,33 @@ package data.structure.link.quick_slow;
  * ░     ░ ░      ░  ░
  *
  * @author ：涂齐康
- * @date ：Created in 2019-06-18 14:31
- * @description：
+ * @date ：Created in 2019-06-19 13:22
+ * leetcode142
+ * @description：给定一个链表，返回链表开始入环的第一个节点。 如果链表无环，则返回 null。
  * @modified By：
- * leetcode141
- * 判断单链表是否存在环
  * @version:
  */
-public class HasCircle {
-    static boolean hasCircle(Node head) {
-        if (head == null) {
-            return false;
-        }
-        Node slow = head;
+public class DetectCycle {
+
+    public static Node detectCycle(Node head) {
         Node fast = head;
+        Node slow = head;
+        boolean nonCircle = true;
         while (fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
             if (slow == fast) {
-                return true;
+                nonCircle = false;
+                break;
             }
         }
-
-        return false;
-    }
-
-    public static void main(String[] args) {
-        System.out.println("生成一个有环的单链表");
-        if (hasCircle(LinkUtils.getCircle())) {
-            System.out.println("有环");
-        } else {
-            System.out.println("无环");
+        if (nonCircle) {
+            return null;
         }
-
-        System.out.println("生成一个无环的单链表");
-        if (hasCircle(LinkUtils.getNonCircle())) {
-            System.out.println("有环");
-        } else {
-            System.out.println("无环");
+        while (slow != head) {
+            head = head.next;
+            slow = slow.next;
         }
+        return head;
     }
 }
