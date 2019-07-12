653068422c4d2c87ad58f67a2d6f6d797afab2b2
tqk <1282995880@qq.com>
2019-07-12 16:41:23 +0800

    LeetCode每日一题

---
+++ src/main/java/leetcode/LeetCode700.java
@@ -0,0 +1,44 @@
+package leetcode;
+
+/**
+ * █████▒█      ██  ▄████▄   ██ ▄█▀       ██████╗ ██╗   ██╗ ██████╗
+ * ▓██   ▒ ██  ▓██▒▒██▀ ▀█   ██▄█▒        ██╔══██╗██║   ██║██╔════╝
+ * ▒████ ░▓██  ▒██░▒▓█    ▄ ▓███▄░        ██████╔╝██║   ██║██║  ███╗
+ * ░▓█▒  ░▓▓█  ░██░▒▓▓▄ ▄██▒▓██ █▄        ██╔══██╗██║   ██║██║   ██║
+ * ░▒█░   ▒▒█████▓ ▒ ▓███▀ ░▒██▒ █▄       ██████╔╝╚██████╔╝╚██████╔╝
+ * ▒ ░   ░▒▓▒ ▒ ▒ ░ ░▒ ▒  ░▒ ▒▒ ▓▒       ╚═════╝  ╚═════╝  ╚═════╝
+ * ░     ░░▒░ ░ ░   ░  ▒   ░ ░▒ ▒░
+ * ░ ░    ░░░ ░ ░ ░        ░ ░░ ░
+ * ░     ░ ░      ░  ░
+ *
+ * @author ：涂齐康
+ * @date ：Created in 2019-07-12 16:34
+ * @description：
+ * @modified By：
+ * @version:
+ */
+public class LeetCode700 {
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public TreeNode searchBST(TreeNode root, int val) {
+        while (true){
+            if (root==null){
+                return null;
+            }else if (root.val==val){
+                return root;
+            }else if (root.val>val){
+                root=root.left;
+            }else {
+                root=root.right;
+            }
+        }
+    }
+}
