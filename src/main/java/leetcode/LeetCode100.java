361f883a84174b1b954caadfe000d9ff92372523
tqk <1282995880@qq.com>
2019-07-21 17:41:34 +0800

    前几天和朋友去玩了，现在重新开始leetcode

---
+++ src/main/java/leetcode/LeetCode100.java
@@ -0,0 +1,60 @@
+package leetcode;
+
+import data.structure.tree.BinarySearchTree;
+
+import java.util.Stack;
+
+/**
+ * █████▒█      ██  ▄████▄   ██ ▄█▀       ██████╗ ██╗   ██╗ ██████╗
+ * ▓██   ▒ ██  ▓██▒▒██▀ ▀█   ██▄█▒        ██╔══██╗██║   ██║██╔════╝
+ * ▒████ ░▓██  ▒██░▒▓█    ▄ ▓███▄░        ██████╔╝██║   ██║██║  ███╗
+ * ░▓█▒  ░▓▓█  ░██░▒▓▓▄ ▄██▒▓██ █▄        ██╔══██╗██║   ██║██║   ██║
+ * ░▒█░   ▒▒█████▓ ▒ ▓███▀ ░▒██▒ █▄       ██████╔╝╚██████╔╝╚██████╔╝
+ * ▒ ░   ░▒▓▒ ▒ ▒ ░ ░▒ ▒  ░▒ ▒▒ ▓▒       ╚═════╝  ╚═════╝  ╚═════╝
+ * ░     ░░▒░ ░ ░   ░  ▒   ░ ░▒ ▒░
+ * ░ ░    ░░░ ░ ░ ░        ░ ░░ ░
+ * ░     ░ ░      ░  ░
+ *
+ * @author ：涂齐康
+ * @date ：Created in 2019-07-21 17:26
+ * @description：相同的树
+ * @modified By：
+ * @version:
+ */
+public class LeetCode100 {
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        return pre(p).equals(pre(q));
+    }
+
+    public String pre(TreeNode p) {
+        StringBuilder sb = new StringBuilder();
+        Stack<TreeNode> stack = new Stack<>();
+        while (true) {
+            while (p != null) {
+                sb.append(p.val).append('-');
+                stack.push(p);
+                p = p.left;
+            }
+            sb.append('n').append('-');
+            if (stack.isEmpty()) {
+                break;
+            }
+            p = stack.pop();
+            p = p.right;
+        }
+        return sb.toString();
+    }
+
+
+}
